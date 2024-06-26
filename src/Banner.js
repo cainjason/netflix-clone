5a67b67b182f8faa2fdccf05e2436eafa9671a53
Jason Cain <118203097+cainjason@users.noreply.github.com>
2024-06-26 00:20:07 -0400

    created banner buttons

--- src/Banner.js
+++ src/Banner.js
@@ -10,7 +10,18 @@ function Banner() {
         backgroundImage: `url('https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Ftelevision.mxdwn.com%2Fwp-content%2Fuploads%2F2015%2F08%2Fnetflix-banner.jpg&f=1&nofb=1&ipt=3f75ea8d91aa99e3f4f22637e901f24b42cc5c74a17270e06b07398c2edfd02c&ipo=images')`,
         backgroundPosition: "center center",
       }}
-    ></header>
+    >
+      <div className="banner__contents">
+        <h1 className="banner__title">Movie Name</h1>
+        <div className="banner__buttons">
+          <button className="banner__button">Play</button>
+          <button className="banner__button">My List</button>
+        </div>
+        <h1 className="banner__description">This is a test description</h1>
+      </div>
+
+      <div className="banner--fadeBottom"></div>
+    </header>
   );
 }
 
