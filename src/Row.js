b981cfb70979d784250ae40868b32bdec7b7a530
Jason Cain <118203097+cainjason@users.noreply.github.com>
2024-06-28 01:56:27 -0400

    base_url

--- src/Row.js
+++ src/Row.js
@@ -5,6 +5,8 @@ import axios from "./axios";
 function Row({ title, fetchUrl, isLargeRow = false }) {
   const [movies, setMovies] = useState([]);
 
+  const base_url = "https://image.tmdb.org/t/p/original/";
+
   useEffect(() => {
     async function fetchData() {
       const request = await axios.get(fetchUrl);
@@ -21,7 +23,12 @@ function Row({ title, fetchUrl, isLargeRow = false }) {
       <h2>{title}</h2>
 
       {movies.map((movie) => (
-        <img src={``} alt="" />
+        <img
+          src={`${base_url}${
+            isLargeRow ? movies.poster_path : movie.backdrop_path
+          }`}
+          alt={movie.name}
+        />
       ))}
     </div>
   );
