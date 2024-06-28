4be7a84ab08f36c679343dc66209eb55e1ed08ab
Jason Cain <118203097+cainjason@users.noreply.github.com>
2024-06-28 01:08:40 -0400

    set request data

--- src/Row.js
+++ src/Row.js
@@ -1,8 +1,23 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Row.css";
+import axios from "./axios";
 
-function Row() {
-  return <div className="row"></div>;
+function Row({ title, fetchUrl, isLargeRow = false }) {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const request = await axios.get(fetchUrl);
+      setMovies(request.data.results);
+      return request;
+    }
+  }, [fetchUrl]);
+
+  return (
+    <div className="row">
+      <h2>{title}</h2>
+    </div>
+  );
 }
 
 export default Row;
